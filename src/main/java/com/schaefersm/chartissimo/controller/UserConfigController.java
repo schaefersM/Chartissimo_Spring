b9649f8fa175c553b1873f85c03c523b6dd43144
Max Schäfers <maximilian.schaefers@gmx.de>
2021-05-23 10:49:24 +0200

    Implement UserConfigController

--- src/main/java/com/schaefersm/chartissimo/controller/UserConfigController.java
+++ src/main/java/com/schaefersm/chartissimo/controller/UserConfigController.java
@@ -1,7 +1,11 @@
 package com.schaefersm.chartissimo.controller;
 
-import java.util.Optional;
-
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +21,24 @@ public class UserConfigController {
 
 	private UserConfigRepository userConfigRepository;
 
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
 	public UserConfigController(UserConfigRepository userConfigRepository) {
 		this.userConfigRepository = userConfigRepository;
 	}
 
 	@GetMapping("/{userId}/config")
-	public Optional<UserConfig> getUserConfig(@PathVariable String userId) {
-		System.out.println(userId);
-		Optional<UserConfig> userConfig = this.userConfigRepository.findByUserId(userId);
-		return userConfig;
+	public ResponseEntity<UserConfig> findById(@PathVariable("userId") ObjectId userId) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("user").is(userId));
+		System.out.println(query);
+		UserConfig userconfig = mongoTemplate.findOne(query, UserConfig.class);
+		if (userconfig != null) {
+			return ResponseEntity.ok(userconfig);
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
+
 }
