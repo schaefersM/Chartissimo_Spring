22c17f3591368732442f1378314648e9f0aebf45
schaefersM <maximilian.schaefers@gmx.de>
2021-11-11 00:19:28 +0100

    Add: Dataset base class

--- spring/data/src/main/java/com/schaefersm/chartissimo/model/Dataset.java
+++ spring/data/src/main/java/com/schaefersm/chartissimo/model/Dataset.java
@@ -1,5 +1,7 @@
 package com.schaefersm.chartissimo.model;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
 import javax.persistence.Column;
@@ -8,62 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Data
 public class Dataset {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "host")
-    private String host;
-    
-    @Column(name = "value")
-    private int value;
-
-    @Column(name = "hour")
-    private int hour;
-
-    @Column(name = "minute")
-    private int minute;
-
-    @Column(name = "datum")
-    private LocalDate datum;
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getHost() {
-        return host;
-    }
-    public void setHost(String host) {
-        this.host = host;
-    }
-    public int getValue() {
-        return value;
-    }
-    public void setValue(int value) {
-        this.value = value;
-    }
-    public int getHour() {
-        return hour;
-    }
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-    public int getMinute() {
-        return minute;
-    }
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
-    public LocalDate getDate() {
-        return datum;
-    }
-    public void setDate(LocalDate datum) {
-        this.datum = datum;
-    }
 }
