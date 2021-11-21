09eaca4b417d3a1d4101e51793060c1c139ef94c
schaefersM <maximilian.schaefers@gmx.de>
2021-11-21 02:27:29 +0100

    Add: Gateway Application

---
+++ spring/gateway/src/main/java/com/schaefersm/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.schaefersm.gateway;
+
+import com.schaefersm.gateway.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+
+}
