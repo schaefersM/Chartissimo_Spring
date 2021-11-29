55e159711d0f7dc81a13f9ca1d081924f509e6da
schaefersM <maximilian.schaefers@gmx.de>
2021-11-30 00:18:36 +0100

    Delete: Cors options

--- spring/user/src/main/java/com/schaefersm/chartissimo/controller/UserChartController.java
+++ spring/user/src/main/java/com/schaefersm/chartissimo/controller/UserChartController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://localhost")
 public class UserChartController {
 
 	@Autowired
